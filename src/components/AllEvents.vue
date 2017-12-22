5f3fb089de1e58230f60a9fae5ca1173af54d964
Jarek Lipski <jlipski@xebia.fr>
2017-12-22 21:48:53 +0100

    Infinite scroll simple

--- src/components/AllEvents.vue
+++ src/components/AllEvents.vue
@@ -4,18 +4,27 @@
       <event :event="event"></event>
       <v-divider v-if="index + 1 < events.length"></v-divider>
     </template>
+    <infinite-loading @infinite="infiniteLoad">
+      <span slot="no-results"></span>
+      <span slot="no-more"></span>
+    </infinite-loading>
   </v-list>
 </template>
 
 <script>
+import InfiniteLoading from 'vue-infinite-loading'
+
 import {getAllEvents} from '../data/schedule'
 
 import Event from './Event'
+
+const PAGE_SIZE = 25
 
 export default {
   name: 'all-events',
 
   components: {
+    InfiniteLoading,
     'event': Event
   },
 
@@ -23,8 +32,22 @@ export default {
     events: []
   }),
 
+  methods: {
+    infiniteLoad (state) {
+      return getAllEvents().then(events => {
+        this.events.push(...events.slice(this.events.length, this.events.length + PAGE_SIZE))
+
+        if (this.events.length < events.length) {
+          return state.loaded()
+        } else {
+          return state.complete()
+        }
+      })
+    }
+  },
+
   created: function () {
-    getAllEvents().then(events => { this.events = events })
+    getAllEvents().then(events => { this.events = events.slice(0, PAGE_SIZE) })
   }
 }
 </script>
