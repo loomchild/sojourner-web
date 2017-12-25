fead0655add737346a3003164d164ce74ac3be3c
Jarek Lipski <loomchild@loomchild.net>
2017-12-25 13:34:28 +0100

    Remove infinite scroll

--- src/components/AllEvents.vue
+++ src/components/AllEvents.vue
@@ -5,28 +5,19 @@
         <event :event="event"></event>
         <v-divider v-if="index + 1 < events.length"></v-divider>
       </template>
-      <infinite-loading @infinite="infiniteLoad" spinner="waveDots">
-        <span slot="no-results"></span>
-        <span slot="no-more"></span>
-      </infinite-loading>
     </v-list>
   </v-layout>
 </template>
 
 <script>
-import InfiniteLoading from 'vue-infinite-loading'
-
 import {getAllEvents} from '../data/schedule'
 
 import Event from './Event'
-
-const PAGE_SIZE = 50
 
 export default {
   name: 'all-events',
 
   components: {
-    InfiniteLoading,
     'event': Event
   },
 
@@ -34,21 +25,8 @@ export default {
     events: []
   }),
 
-  methods: {
-    infiniteLoad (state) {
-      return getAllEvents().then(events => {
-        this.events.push(...events.slice(this.events.length, this.events.length + PAGE_SIZE))
-        if (this.events.length < events.length) {
-          return state.loaded()
-        } else {
-          return state.complete()
-        }
-      })
-    }
-  },
-
   created: function () {
-    getAllEvents().then(events => { this.events = events.slice(0, PAGE_SIZE) })
+    getAllEvents().then(events => { this.events = events })
   }
 }
 </script>
