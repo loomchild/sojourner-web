7451b0976ccc779b78f4400a5606c3a95c8cbcdb
Jarek Lipski <loomchild@loomchild.net>
2017-12-25 14:48:23 +0100

    Stub schedule in unit tests

--- test/unit/data/schedule.test.js
+++ test/unit/data/schedule.test.js
@@ -1,6 +1,20 @@
+import sinon from 'sinon'
+
 import {getSchedule, getAllEvents} from '../../../src/data/schedule'
+import xml from '../resources/schedule.xml'
 
 describe('data/getSchedule', () => {
+  let stubbedFetch
+
+  beforeEach(() => {
+    stubbedFetch = sinon.stub(window, 'fetch')
+    fetch.resolves(new Response(xml, {status: 200}))
+  })
+
+  afterEach(() => {
+    stubbedFetch.restore()
+  })
+
   it('should retrieve schedule', () => {
     return getSchedule()
       .then(schedule => {
---
+++ test/unit/resources/schedule.xml.js
@@ -0,0 +1,57 @@
+export default `<?xml version="1.0" encoding="UTF-8"?>
+<schedule>
+  <conference>
+    <title>FOSDEM 2018</title>
+    <subtitle/>
+    <venue>ULB (Université Libre de Bruxelles)</venue>
+    <city>Brussels</city>
+    <start>2018-02-03</start>
+    <end>2018-02-04</end>
+    <days>2</days>
+    <day_change>09:00:00</day_change>
+    <timeslot_duration>00:05:00</timeslot_duration>
+  </conference>
+  <day index="1" date="2018-02-03">
+    <room name="Janson">
+      <event id="6245">
+        <start>10:00</start>
+        <duration>00:50</duration>
+        <room>Janson</room>
+        <slug>osi</slug>
+        <title>Consensus as a Service</title>
+        <subtitle>Twenty Years of OSI Stewardship</subtitle>
+        <track>Keynotes</track>
+        <type>keynote</type>
+        <language/>
+        <abstract>The Open Source label was born on February 3, 1998, and will celebrate its 20th Anniversary on February 3, 2018, during the opening day of FOSDEM 2018.</abstract>
+        <description>&lt;p&gt;Open source software is now ubiquitous, recognized across industries as a fundamental component to infrastructure, as well as a critical factor for driving innovation.&lt;/p&gt;</description>
+        <persons>
+          <person id="228">Simon Phipps</person>
+          <person id="312">Italo Vignoli</person>
+        </persons>
+        <links>
+          <link href="https://submission.fosdem.org/feedback/6245.php">Submit feedback</link>
+        </links>
+      </event>
+      <event id="6230">
+        <start>12:00</start>
+        <duration>00:50</duration>
+        <room>Janson</room>
+        <slug>unix_evolution</slug>
+        <title>Unix Architecture Evolution from the 1970 PDP-7 to the 2017 FreeBSD</title>
+        <subtitle>Important Milestones and Lessons Learned</subtitle>
+        <track>History</track>
+        <type>maintrack</type>
+        <language/>
+        <description>The Unix operating system has had a profound influence on the development of open source software and associated communities.</description>
+        <persons>
+          <person id="3691">Diomidis Spinellis</person>
+        </persons>
+        <links>
+          <link href="https://dspinellis.github.io/unix-architecture/arch-V1.pdf">First Research Edition architecture diagram</link>
+        </links>
+      </event>
+    </room>
+  </day>
+</schedule>
+`
