9b739add2da16e59e8c69b37f56c279fde4d05e4
Jarek Lipski <loomchild@loomchild.net>
2017-12-31 20:30:17 +0100

    Deploy with timestamp

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,11 @@ if [[ $STATUS == *"nothing to commit, working tree clean"* ]]
 then
     VERSION=`git describe  --long --match "[0-9].*"`
     TIMESTAMP=`date +'%Y-%m-%d %H:%M:%S'`
-    sed -i "/version: '.*'/version: '$VERSION'/" config.js
-    sed -i "/timestamp: '.*'/timestamp: '$TIMESTAMP'/" config.js
+    sed -i "s/version: '.*'/version: '$VERSION'/" config.js
+    sed -i "s/timestamp: '.*'/timestamp: '$TIMESTAMP'/" config.js
+
+    npm run build
+
     sed -i '/dist/d' .gitignore
     git add .
     git commit -m "Deploy"
