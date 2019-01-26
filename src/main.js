9b14e7480502f64617b429d6182f35783a003b65
Jarek Lipski <loomchild@loomchild.net>
2019-01-26 15:19:21 +0100

    fix vuetify/icon import order, fix fab

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'vuetify/dist/vuetify.min.css'
 import 'typeface-roboto'
 
 import App from './App'
