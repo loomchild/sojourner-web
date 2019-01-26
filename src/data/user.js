e6756eb8e00d2b3c20e906e16d770b444a7ff759
Jarek Lipski <loomchild@loomchild.net>
2019-01-26 18:00:46 +0100

    log current user ID

--- src/data/user.js
+++ src/data/user.js
@@ -54,6 +54,7 @@ export default {
 
     initUser ({commit, dispatch}, user) {
       return firebase.auth().onAuthStateChanged(user => {
+        console.log(`Initializing user ${user.uid}`)
         commit('setUser', user)
         if (user) {
           return dispatch('initFavourites')
