8608d96ed9e4ca8e024b4009b94faa6e92a3b871
Jarek Lipski <loomchild@loomchild.net>
2019-05-29 19:45:11 +0200

    fix bottom-nav hiding

--- components/BottomMenu.vue
+++ components/BottomMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-bottom-nav id="bottom-menu" class="hidden-md-and-up" app fixed :value="true" color="secondary">
+  <v-bottom-nav id="bottom-menu" app fixed :value="$vuetify.breakpoint.smAndDown" color="secondary">
     <v-btn color="white" flat to="/">
       <v-icon>home</v-icon>
     </v-btn>
