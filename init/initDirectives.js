aab46733c7d2c6288a9a70f72bbf77058983f7ef
Jarek Lipski <loomchild@loomchild.net>
2019-05-29 20:12:51 +0200

    remove console log for directive

--- init/initDirectives.js
+++ init/initDirectives.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 
 Vue.directive('go', function (el, binding, vnode) {
-  console.log(binding)
   el.addEventListener('click', () => {
     setTimeout(() => vnode.context.$router.push(binding.value), 150)
   })
