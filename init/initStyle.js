a7499daaae53daf30ac5768c89f675ec52a9a896
Jarek Lipski <loomchild@loomchild.net>
2019-05-29 17:51:54 +0200

    add Rubik font

--- init/initStyle.js
+++ init/initStyle.js
@@ -3,6 +3,7 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'typeface-roboto'
+import 'typeface-rubik'
 
 import '@/preload.css'
 
