ec3fa643162389684824047fbf92ffe9cdd59006
Jarek Lipski <loomchild@loomchild.net>
2019-05-30 12:34:29 +0200

    fix v-go directive cleanup

--- init/initDirectives.js
+++ init/initDirectives.js
@@ -1,9 +1,27 @@
 import Vue from 'vue'
 
-Vue.directive('go', function (el, binding, vnode) {
-  el.addEventListener('click', () => {
+function addGoDirective (el, binding, vnode) {
+  const handler = () => {
     setTimeout(() => vnode.context.$router.push(binding.value), 150)
-  })
+  }
+
+  el.addEventListener('click', handler)
+  el.$goCleanup = () => el.removeEventListener('click', handler)
+}
+
+function removeGoDirective (el) {
+  el.$goCleanup()
+}
+
+function updateGoDirective (el, binding, vnode) {
+  removeGoDirective(el)
+  addGoDirective(el, binding, vnode)
+}
+
+Vue.directive('go', {
+  bind: addGoDirective,
+  update: updateGoDirective,
+  unbind: removeGoDirective
 })
 
 export default function () {}
