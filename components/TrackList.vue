975c33ba1002b4d16ba560d7eaf97118afb958f6
Jarek Lipski <loomchild@loomchild.net>
2019-05-31 20:21:01 +0200

    common track list

---
+++ components/TrackList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-list three-line>
+      <template v-for="(track, index) in tracks">
+        <conference-track :track="track"></conference-track>
+        <v-divider v-if="index + 1 < tracks.length"></v-divider>
+      </template>
+    </v-list>
+  </div>
+</template>
+
+<script>
+import ConferenceTrack from '@/components/ConferenceTrack'
+
+export default {
+  name: 'tracks',
+
+  components: {
+    'conference-track': ConferenceTrack
+  },
+
+  props: [
+    'tracks'
+  ]
+}
+</script>
+
+<style>
+</style>
--- pages/TypeTracksOrEvents.vue
+++ pages/TypeTracksOrEvents.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <v-list v-if="tracks.length > 1" three-line>
-      <template v-for="(track, index) in tracks">
-        <conference-track :track="track"></conference-track>
-        <v-divider v-if="index + 1 < tracks.length"></v-divider>
-      </template>
-    </v-list>
+    <track-list v-if="tracks.length > 1" :tracks="tracks"></track-list>
     <event-list v-if="tracks.length === 1" :events="events"></event-list>
   </div>
 </template>
@@ -13,14 +8,14 @@
 <script>
 import {mapGetters} from 'vuex'
 
-import ConferenceTrack from '@/components/ConferenceTrack'
+import TrackList from '@/components/TrackList'
 import EventList from '@/components/EventList'
 
 export default {
   name: 'type-tracks-or-events',
 
   components: {
-    'conference-track': ConferenceTrack,
+    'track-list': TrackList,
     'event-list': EventList
   },
 
