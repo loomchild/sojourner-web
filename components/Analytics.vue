af876239a653d6d4eb032390152ee62a95133582
Jarek Lipski <loomchild@loomchild.net>
2019-06-10 16:47:27 +0200

    make pixel absolute to not interfere with the layout

--- components/Analytics.vue
+++ components/Analytics.vue
@@ -1,5 +1,5 @@
 <template>
-  <img v-if="analyticsImageUrl" :src="analyticsImageUrl" style="border:0" alt="" />
+  <img v-if="analyticsImageUrl" :src="analyticsImageUrl" class="pixel" style="border:0" alt="" />
 </template>
 
 <script>
@@ -15,4 +15,10 @@ export default {
 </script>
 
 <style>
+.pixel {
+  border: 0;
+  position: absolute;
+  width: 1px;
+  height: 1px;
+}
 </style>
--- init/initFixes.js
+++ init/initFixes.js
@@ -1,14 +1,8 @@
 function updateVh () {
   setTimeout(() => {
     const screenHeight = window.innerHeight
-    console.log(screenHeight)
     document.documentElement.style.setProperty('--screenHeight', `${screenHeight}px`)
   }, 100)
-  setTimeout(() => {
-    const screenHeight = window.innerHeight
-    console.log(screenHeight)
-    document.documentElement.style.setProperty('--screenHeight', `${screenHeight}px`)
-  }, 1000)
 }
 
 export default function () {
