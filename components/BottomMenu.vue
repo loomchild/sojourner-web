0bc1168f33c6290bdf1d34196668aeb451817701
Jarek Lipski <loomchild@loomchild.net>
2019-06-10 18:12:51 +0200

    fix bottom menu colors

--- components/BottomMenu.vue
+++ components/BottomMenu.vue
@@ -1,18 +1,18 @@
 <template>
-  <v-bottom-nav id="bottom-menu" app fixed :value="$vuetify.breakpoint.smAndDown" color="secondary">
-    <v-btn color="white" flat to="/">
+  <v-bottom-nav dark id="bottom-menu" app fixed :value="$vuetify.breakpoint.smAndDown" color="secondary">
+    <v-btn flat to="/">
       <v-icon>home</v-icon>
     </v-btn>
 
-    <v-btn color="white" flat to="/favourites/">
+    <v-btn flat to="/favourites/">
       <v-icon>bookmarks</v-icon>
     </v-btn>
 
-    <v-btn color="white" flat to="/map/">
+    <v-btn flat to="/map/">
       <v-icon>map</v-icon>
     </v-btn>
 
-    <v-btn color="white" flat to="/search/">
+    <v-btn flat to="/search/">
       <v-icon>search</v-icon>
     </v-btn>
   </v-bottom-nav>
