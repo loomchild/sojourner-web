beae27bcca5f6c3793bcac6102dc65c6e9c5764b
Jarek Lipski <loomchild@loomchild.net>
2019-06-12 17:20:29 +0200

    remove map tip

--- pages/CampusMap.vue
+++ pages/CampusMap.vue
@@ -13,18 +13,8 @@
 </template>
 
 <script>
-import {mapActions} from 'vuex'
-
 export default {
   name: 'campus-map',
-
-  methods: mapActions([
-    'showZoomTip'
-  ]),
-
-  created () {
-    this.showZoomTip()
-  },
 
   metaInfo () {
     return {
--- store/map.js
+++ store/map.js
@@ -8,36 +8,13 @@ export default {
       'AW': new Building({name: 'AW'}),
       'U': new Building({name: 'U'}),
       'K': new Building({name: 'K'})
-    },
-
-    zoomTipShown: false
+    }
   },
 
   getters: {
     buildings: state => state.buildings,
 
-    roomBuilding: state => roomName => Object.values(state.buildings).filter(building => building.containsRoom(roomName))[0],
-
-    zoomTipShown: state => state.zoomTipShown
-  },
-
-  mutations: {
-    setZoomTipShown (state, zoomTipShown) {
-      state.zoomTipShown = zoomTipShown
-    }
-  },
-
-  actions: {
-    showZoomTip ({commit, state, dispatch}) {
-      if (!state.zoomTipShown) {
-        dispatch('showNotification', {
-          message: 'Click on a building to zoom in',
-          color: 'info',
-          timeout: 10000
-        })
-        commit('setZoomTipShown', true)
-      }
-    }
+    roomBuilding: state => roomName => Object.values(state.buildings).filter(building => building.containsRoom(roomName))[0]
   }
 
 }
