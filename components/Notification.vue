b0363db9f88adcaa83c836c389e0972201c7aa1c
Jarek Lipski <loomchild@loomchild.net>
2019-06-20 20:43:41 +0200

    move notification buttons to the right

--- components/Notification.vue
+++ components/Notification.vue
@@ -1,8 +1,9 @@
 <template>
-  <v-snackbar :timeout="notification.timeout" :color="notification.color" bottom multi-line absolute :value="notificationVisible" @change="setNotificationVisible">
+  <v-snackbar :timeout="notification.timeout" :color="notification.color" bottom multi-line :value="notificationVisible" @change="setNotificationVisible">
     {{ notification.message }}
-    <v-btn v-if="notification.button" flat small right @click.native="setNotificationVisible(false); notification.button.handler();">{{ notification.button.title }}</v-btn>
-    <v-btn flat small right icon @click.native="setNotificationVisible(false)"><v-icon>close</v-icon></v-btn>
+    <v-spacer></v-spacer>
+    <v-btn v-if="notification.button" flat small @click.native="setNotificationVisible(false); notification.button.handler();">{{ notification.button.title }}</v-btn>
+    <v-btn flat small icon @click.native="setNotificationVisible(false)"><v-icon>close</v-icon></v-btn>
   </v-snackbar>
 </template>
 
@@ -27,10 +28,4 @@ export default {
 .v-snack button {
   margin-left: 10px !important;
 }
-
-.v-snack .v-btn--right {
-  right: 0;
-}
 </style>
-
-
