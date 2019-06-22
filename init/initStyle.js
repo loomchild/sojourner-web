a636283b7b01ca75d973d88589d676f5bec63bd0
Jarek Lipski <loomchild@loomchild.net>
2019-06-22 12:59:06 +0200

    remove roboto font

--- init/initStyle.js
+++ init/initStyle.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import 'typeface-roboto'
 import 'typeface-rubik'
 import '@/main.styl'
 
