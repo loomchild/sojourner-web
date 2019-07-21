be91065489d9127e40e09969b1aaf16bbf0d0dfb
Jarek Lipski <loomchild@loomchild.net>
2019-07-21 19:22:56 +0200

    do not cut day on search

--- pages/SearchEvents.vue
+++ pages/SearchEvents.vue
@@ -2,26 +2,24 @@
   <v-container fluid class="content">
     <page-title></page-title>
     <v-text-field ref="search" solo clearable hide-details placeholder="Enter your keywords" prepend-inner-icon="search" v-model="query" autocapitalize="none" color="secondary"/>
-    <v-layout align-top>
-      <v-list v-if="validQuery && events.length > 0" three-line class="pa-0">
-        <template v-for="(event, index) in events">
-          <event :event="event" show-day></event>
-          <v-divider v-if="index + 1 < events.length"></v-divider>
-        </template>
-      </v-list>
-      <v-list v-if="validQuery && events.length == 0" >
-        <v-list-tile>
-          <v-list-tile-content>
-            <v-list-tile-sub-title>
-              No events found.
-            </v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-      <div v-if="!validQuery" class="search-help">
-        <v-icon>subdirectory_arrow_left</v-icon> Start typing to search for an event by title, description, track, speaker, room, etc.
-      </div>
-    </v-layout>
+    <v-list v-if="validQuery && events.length > 0" three-line class="pa-0">
+      <template v-for="(event, index) in events">
+        <event :event="event" show-day></event>
+        <v-divider v-if="index + 1 < events.length"></v-divider>
+      </template>
+    </v-list>
+    <v-list v-if="validQuery && events.length == 0" >
+      <v-list-tile>
+        <v-list-tile-content>
+          <v-list-tile-sub-title>
+            No events found.
+          </v-list-tile-sub-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+    <div v-if="!validQuery" class="search-help">
+      <v-icon>subdirectory_arrow_left</v-icon> Start typing to search for an event by title, description, track, speaker, room, etc.
+    </div>
     <div class="hidden-sm-and-down mb-5"></div>
   </v-container>
 </template>
