3ef8e4f6dc5be04e9dd86fbac6c2a89440d234a2
Jarek Lipski <loomchild@loomchild.net>
2019-07-21 21:37:37 +0200

    fix desktop About, simple mobile

--- pages/About.vue
+++ pages/About.vue
@@ -1,47 +1,34 @@
 <template>
-  <v-container v-if="$vuetify.breakpoint.smAndDown" fluid fill-height class="content">
-    <v-layout row wrap>
-      <v-layout row wrap>
-        <v-flex sm12 md6 offset-md3>
-          <div class="body-2">
-            <p><v-icon>book</v-icon> Version: <a :href="commiturl">{{ version }}</a></p>
-            <p><v-icon>alarm</v-icon> Timestamp: {{ timestamp }}</p>
-            <p><v-icon>fingerprint</v-icon> Author: <a href="https://studio.untitledfactory.com" target="_blank">Untitled Factory Studio</a></p>
-            <p><v-icon>email</v-icon> Support: <a href="mailto:contact@untitledfactory.com?subject=Sojourner">contact@untitledfactory.com</a></p>
-            <p><v-icon>code</v-icon> Code: <a href="https://github.com/loomchild/sojourner-web">Github</a></p>
-            <p>Sojourner is a conference schedule viewer. It is inspired by the original <a href="https://github.com/loomchild/sojourner">Sojourner</a> mobile app for Nokia N900.</p>
-          </div>
-        </v-flex>
-      </v-layout>
-    </v-layout>
-  </v-container>
-  <v-container v-else fluid fill-height grid-list-xl class="content px-3">
+  <v-container fluid fill-height class="content">
     <v-layout column>
-      <v-flex lg6 xs7 class="mt-auto">
-        <h1 class="page-title">About</h1>
-        <v-layout row wrap>
-          <v-flex xs12 class="headline">
-            <p class="mb-1">
-              <span class="secondary--text">Author:</span>
-              <span class="white--text"><a href="https://studio.untitledfactory.com" target="_blank">Untitled Factory Studio</a></span>
-            </p>
-            <p class="mb-1">
-              <span class="secondary--text">Support:</span>
-              <span class="white--text"><a href="mailto:contact@untitledfactory.com?subject=Sojourner">contact@untitledfactory.com</a></span>
-            </p>
-            <p class="mb-1">
-              <span class="secondary--text">Version:</span>
-              <span class="white--text"><a :href="commiturl">{{ version }} / {{ timestamp }}</a></span>
-            </p>
-            <p class="mb-1">
-              <span class="secondary--text">Code:</span>
-              <span class="white--text"><a href="https://github.com/loomchild/sojourner-web">Github</a></span>
-            </p>
-          </v-flex>
-        </v-layout>
-        <v-layout row wrap>
-          <v-flex xs9 class="title font-weight-regular pt-0">
-            <p class="secondary--text story">Sojourner is a conference companion. It is inspired by the original <a href="https://github.com/loomchild/sojourner">Sojourner</a> mobile app for Nokia N900.</p>
+      <v-flex shrink>
+        <page-title></page-title>
+      </v-flex>
+      <v-flex grow class="title font-weight-regular white--text">
+        <p class="mb-2">
+          <span>Author:</span>
+          <span><a href="https://studio.untitledfactory.com" target="_blank">Untitled Factory Studio</a></span>
+        </p>
+        <p class="mb-2">
+          <span>Support:</span>
+          <span><a href="mailto:contact@untitledfactory.com?subject=Sojourner">contact@untitledfactory.com</a></span>
+        </p>
+        <p class="mb-2">
+          <span>Version:</span>
+          <span><a :href="commiturl">{{ version }}</a></span>
+        </p>
+        <p class="mb-2">
+          <span>Updated:</span>
+          <span><a :href="commiturl">{{ timestamp }}</a></span>
+        </p>
+        <p class="mb-2">
+          <span>Code:</span>
+          <span><a href="https://github.com/loomchild/sojourner-web">Github</a></span>
+        </p>
+        <v-layout row>
+          <v-flex xs12 sm9 class="mt-4 title font-weight-regular white--text">
+            <p class="story">Sojourner is a conference companion. It is inspired by the original <a href="https://github.com/loomchild/sojour
+    ner">Sojourner</a> mobile app for Nokia N900.</p>
           </v-flex>
         </v-layout>
       </v-flex>
@@ -51,9 +38,14 @@
 
 <script>
 import {mapGetters} from 'vuex'
+import PageTitle from '@/components/PageTitle'
 
 export default {
   name: 'about',
+
+  components: {
+    PageTitle
+  },
 
   computed: {
     commiturl () {
@@ -88,22 +80,30 @@ a {
 .story a {
   font-weight: 600;
 }
-</style>
 
-<style>
-.application.about {
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
+.content > .layout {
+  padding: 1rem;
 }
 
 @media only screen and (min-width:960px) {
-  .application.about {
-    background-color: var(--v-primary-base) !important;
-    background-image: url(~assets/dashboard-desktop.png);
-    background-size: 33%;
-    background-position: right bottom;
+  .content > .layout {
+    padding: 0;
   }
 }
 </style>
 
+<style>
+.application.about {
+  background-color: var(--v-primary-base) !important;
+  background-image: url(~assets/dashboard-desktop.png);
+  background-size: 100%;
+  background-position: right bottom;
+  background-repeat: no-repeat;
+}
+
+@media (min-width:960px), (max-height:600px) {
+  .application.about {
+    background-size: 33%;
+  }
+}
+</style>
