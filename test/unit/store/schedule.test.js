df5af159e95149d6f2c513ccad10d9d794d3afc4
Jarek Lipski <loomchild@loomchild.net>
2019-10-31 12:43:00 +0100

    fix schedule test

--- test/unit/store/schedule.test.js
+++ test/unit/store/schedule.test.js
@@ -1,14 +1,14 @@
 import sinon from 'sinon'
 
 import store from '@/store'
-import xml from '@/test/unit/resources/schedule.xml'
+import json from '@/test/unit/resources/fosdem-2019.json'
 
 describe('data/getSchedule', () => {
   let stubbedFetch
 
   beforeEach(() => {
     stubbedFetch = sinon.stub(window, 'fetch')
-    fetch.resolves(new Response(xml, {status: 200}))
+    fetch.resolves(new Response(JSON.stringify(json), {status: 200}))
   })
 
   afterEach(() => {
@@ -18,7 +18,7 @@ describe('data/getSchedule', () => {
   it('should retrieve all events', () => {
     return store.dispatch('initSchedule')
       .then(() => {
-        expect(store.state.schedule.events['6230']).to.not.be.undefined
+        expect(store.state.schedule.events['8967']).to.not.be.undefined
       })
   })
 })
