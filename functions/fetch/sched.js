736cf9eaf99a2587e65d436084f1cb19031e6948
Jarek Lipski <loomchild@loomchild.net>
2019-11-11 19:09:10 +0100

    add sched/FlowCON

---
+++ functions/fetch/sched.js
@@ -0,0 +1,52 @@
+const axios = require('axios')
+const moment = require('moment')
+const Event = require('../logic/Event')
+const Type = require('../logic/Type')
+
+module.exports = async function (scheduleUrl, scheduleKey) {
+  const response = await axios.get(scheduleUrl, { params: { api_key: scheduleKey, format: 'json' } })
+
+  const json = response.data
+
+  const typeMap = {}
+
+  const events = json.map((e) => {
+    const startTime = moment(e.start_time, 'HH:mm:ss')
+    const endTime = moment(e.end_time, 'HH:mm:ss')
+    const duration = moment.utc(endTime.diff(startTime))
+
+    const typeId = e.event_type.toLowerCase().replace(/[^[a-z0-9- ]/g, '').replace(/\s+/g, '-')
+    const type = typeMap[typeId]
+    if (!type) {
+      typeMap[typeId] = {
+        id: typeId,
+        name: e.event_type,
+        sort: e.event_type_sort
+      }
+    }
+
+    const room = e.venue.replace(/\s*-\s*\d+p$/, '')
+
+    return new Event({
+      id: e.id,
+      date: e.start_date,
+      startTime: startTime.format('HH:mm'),
+      duration: duration.format('HH:mm'),
+      title: e.name,
+      description: e.description,
+      type: typeId,
+      room,
+      persons: e.speakers.map(speaker => speaker.name)
+    })
+  })
+
+  const types = Object.values(typeMap)
+    .sort((l, r) => l.sort - r.sort)
+    .map(t => new Type({
+      id: t.id,
+      name: t.name,
+      statName: 'events'
+    }))
+
+  return { types, events }
+}
--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const fetchFosdem = require('./fetch/fosdem')
+const fetchSched = require('./fetch/sched')
 const store = require('./store')
 
 admin.initializeApp({
@@ -12,4 +13,9 @@ admin.initializeApp({
 exports.storeFosdem = functions.pubsub.schedule('every 5 minutes').onRun(async (context) => {
   const fosdemData = await fetchFosdem(process.env.FOSDEM_SCHEDULE_URL)
   await store(fosdemData, 'fosdem-2019.json')
+})
+
+exports.storeFlowcon = functions.pubsub.schedule('every 5 minutes').onRun(async (context) => {
+  const flowconData = await fetchSched(process.env.FLOWCON_SCHEDULE_URL, process.env.FLOWCON_SCHEDULE_KEY)
+  await store(flowconData, 'flowcon-2019.json')
 })
