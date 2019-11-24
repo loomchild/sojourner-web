d3ba0b48962a48c85b05f2c5356cae9f6ab68d57
Jarek Lipski <loomchild@loomchild.net>
2019-11-24 22:54:50 +0100

    fix test-e2e

--- test/e2e/test.js
+++ test/e2e/test.js
@@ -2,8 +2,8 @@ module.exports = {
   'title test': (browser) => {
     browser.url(browser.globals.url)
     browser.waitForElementVisible('body', 5000)
+    browser.waitForElementVisible('h1.page-title', 20000)
 
-    browser.pause(3000) // wait for title update
     browser.assert.title('FOSDEM\'19 | Sojourner')
 
     browser.end()
