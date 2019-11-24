4084ad51ecf5ddd7c4c545686cb14264e7fc9d6d
Jarek Lipski <loomchild@loomchild.net>
2019-11-24 22:45:40 +0100

    wait longer for fosdem title

--- test/e2e/test.js
+++ test/e2e/test.js
@@ -3,7 +3,7 @@ module.exports = {
     browser.url(browser.globals.url)
     browser.waitForElementVisible('body', 5000)
 
-    browser.pause(1000) // wait for title update
+    browser.pause(3000) // wait for title update
     browser.assert.title('FOSDEM\'19 | Sojourner')
 
     browser.end()
