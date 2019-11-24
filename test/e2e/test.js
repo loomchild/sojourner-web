0f6ba7c4298de41caf4a71885b4dd2ddf5eda7bd
Jarek Lipski <loomchild@loomchild.net>
2019-11-24 23:23:06 +0100

    wait even longer for e2e test

--- test/e2e/test.js
+++ test/e2e/test.js
@@ -2,7 +2,7 @@ module.exports = {
   'title test': (browser) => {
     browser.url(browser.globals.url)
     browser.waitForElementVisible('body', 5000)
-    browser.waitForElementVisible('h1.page-title', 20000)
+    browser.waitForElementVisible('h1.page-title', 60000)
 
     browser.assert.title('FLOWCON\'19 | Sojourner')
 
