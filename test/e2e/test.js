ed8d6ed380dbb2506d8d7e1b16c4d4e658d8be02
Jarek Lipski <loomchild@loomchild.net>
2019-12-01 15:24:43 +0100

    increase timeout

--- test/e2e/test.js
+++ test/e2e/test.js
@@ -2,7 +2,7 @@ module.exports = {
   'title test': (browser) => {
     browser.url(browser.globals.url)
     browser.waitForElementVisible('body', 5000)
-    browser.waitForElementVisible('h1.page-title', 10000)
+    browser.waitForElementVisible('h1.page-title', 30000)
 
     browser.assert.titleContains('| Sojourner')
 
