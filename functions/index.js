51292609674da2d69d9aeba314abe37a05dc5610
Jarek Lipski <loomchild@loomchild.net>
2019-12-01 13:08:01 +0100

    do not use dotenv in production

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
