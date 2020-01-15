f5148fe34e813d08f1f618cb57543785df05b8fd
Jarek Lipski <loomchild@loomchild.net>
2020-01-15 13:44:28 +0100

    add video links

--- functions/fetch/sched.js
+++ functions/fetch/sched.js
@@ -11,7 +11,13 @@ const LINKS = {
   'e35739aa2946c4d2c623c8b832f57b56': 'https://www.youtube.com/watch?v=E88pnHQAfm0&list=PLAqbkwjzg7IOr9yJlptM_jMdjIzSONY_1&index=4',
   '52d9910ec3d4907fbb435bf654adf23f': 'https://www.youtube.com/watch?v=P0VMDomQxQk&list=PLAqbkwjzg7IOr9yJlptM_jMdjIzSONY_1&index=5',
   '6b2e0479a790ae48f39191bbb3885ab9': 'https://www.youtube.com/watch?v=re5jkjxBhLo&list=PLAqbkwjzg7IOr9yJlptM_jMdjIzSONY_1&index=6',
-  '8031487dbf3f73a97600831e1cc16cce': 'https://www.youtube.com/watch?v=dEHtDfQTNcw&list=PLAqbkwjzg7IOr9yJlptM_jMdjIzSONY_1&index=7'
+  '8031487dbf3f73a97600831e1cc16cce': 'https://www.youtube.com/watch?v=dEHtDfQTNcw&list=PLAqbkwjzg7IOr9yJlptM_jMdjIzSONY_1&index=7',
+  'b52dd88c2863e9cd65dbf5ca0be2ed76': 'https://www.youtube.com/watch?v=YibNG8xx15c&list=PLAqbkwjzg7IOr9yJlptM_jMdjIzSONY_1&index=9',
+  '3ca106743d1187ca7f45d2843f95a45f': 'https://www.youtube.com/watch?v=RO8vg8MvAtE&list=PLAqbkwjzg7IOr9yJlptM_jMdjIzSONY_1&index=10',
+  'b565f20b2c946608b2b0a329c2a4c2af': 'https://www.youtube.com/watch?v=Fa6cNEB5BMI&list=PLAqbkwjzg7IOr9yJlptM_jMdjIzSONY_1&index=11',
+  '0fb691d344aa0d4d90e1b6be801d3f30': 'https://www.youtube.com/watch?v=iagaLj_HFCQ&list=PLAqbkwjzg7IOr9yJlptM_jMdjIzSONY_1&index=12',
+  'eaf18224a4a4a4956ddb7a767df822d0': 'https://www.youtube.com/watch?v=QsGL1RkmnyU&list=PLAqbkwjzg7IOr9yJlptM_jMdjIzSONY_1&index=13',
+  'e05d0c8e09d002308984f96ff3aa88f8': 'https://www.youtube.com/watch?v=dGKAwRodbjA&list=PLAqbkwjzg7IOr9yJlptM_jMdjIzSONY_1&index=14'
 }
 
 module.exports = async function (scheduleUrl, scheduleKey) {
