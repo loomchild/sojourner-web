f1e5479116337e28c0452786afe5759abc8c78e1
Jarek Lipski <loomchild@loomchild.net>
2020-01-30 20:18:13 +0100

    use local roboto condensed font

--- init/initStyle.js
+++ init/initStyle.js
@@ -3,6 +3,7 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'typeface-rubik'
+import 'typeface-roboto-condensed'
 import '@/main.styl'
 
 import '@/preload.css'
